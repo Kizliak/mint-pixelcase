7fab8ba2435c46cfcd6c816fda3686fb36dbfe21
Andrii Dziubenko <92578692+Kizliak@users.noreply.github.com>
2023-06-17 21:19:11 +0300

    Add files via upload

---
+++ mint-pixelcase.py
@@ -0,0 +1,92 @@
+from web3 import Web3
+import time, random, requests, json
+from decimal import Decimal
+from loguru import logger
+from tqdm import tqdm
+from sys import stderr
+
+############################## Config #################################################
+
+time_delay_min = 750  # Min delay between accs in mili seceonds
+time_delay_max = 1050  # Max delay between accs in mili seceonds
+
+#######################################################################################
+
+logger.remove()
+logger.add(stderr, format="<lm>{time:YYYY-MM-DD HH:mm:ss}</lm> | <level>{level: <8}</level>| <lw>{message}</lw>")
+web3 = Web3(Web3.HTTPProvider('https://mainnet.era.zksync.io'))
+
+mintContract = Web3.to_checksum_address('0x1ec43b024A1C8D084BcfEB2c0548b6661C528dfA')
+
+mintContractAbi = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"contractURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"minted","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"mintedWallets","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"url","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"url","type":"string"}],"name":"setContractURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"timestamp","type":"uint256"}],"name":"setEndTimestamp","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+
+def add_gas_limit(tx):
+    try:
+        gasLimit = web3.eth.estimate_gas(tx)
+        tx['gas'] = int(gasLimit * random.uniform(0.60, 0.61))
+    except:
+        tx['gas'] = random.randint(1500000, 2000000)
+    return tx
+
+def mintNFT(private_key, address_wallet):
+    contract = web3.eth.contract(address=mintContract, abi=mintContractAbi)
+    txData = {
+        'from': address_wallet,
+        'nonce': web3.eth.get_transaction_count(address_wallet),
+        'gasPrice': web3.eth.gas_price,
+        'value': 0
+        }    
+    tx = contract.functions.mint().build_transaction(txData)
+    add_gas_limit(tx)
+    signed_tx = web3.eth.account.sign_transaction(tx, private_key)
+    
+    try:
+        raw_tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_hash = web3.to_hex(raw_tx_hash)
+        tx_receipt = web3.eth.wait_for_transaction_receipt(raw_tx_hash, timeout=600)
+        status = tx_receipt.status
+        if status == 1:
+            logger.success(f'Minted NFT https://explorer.zksync.io/tx/{tx_hash}\n')
+            return True
+        else:
+            if getTxStatus(tx_hash): 
+                logger.success(f'Minted NFT https://explorer.zksync.io/tx/{tx_hash}\n')
+                return True
+            else:
+                logger.error(f'Mint NFT failed. TX status = {status} \n')
+                return False
+    except Exception as e:
+        logger.error(f'Mint NFT failed: {e}\n')
+        return False
+        
+def getTxStatus(tx_hash):
+    time.sleep(30)
+    tx_receipt = web3.eth.get_transaction_receipt(tx_hash)
+    status = tx_receipt.status
+    returnStatus = True if status == 1 else False
+    return returnStatus
+
+def main():
+    print()
+    print('This script will mint NFT at https://zk-mint.pixelcase.io Deadline: 24.06.2023')
+    print()
+
+    with open("private_key.txt", "r") as f:
+        keys_list = [row.strip() for row in f]
+        
+    count_wallets = len(keys_list)
+    number_wallets = 0
+
+    while keys_list:
+        key = keys_list.pop(0)
+        number_wallets += 1
+        address_wallet = web3.eth.account.from_key(key).address
+        print(f'{number_wallets}/{count_wallets} - {address_wallet}\n')
+        mintNFT(key, address_wallet)
+            
+        sleepDelay = random.randint(time_delay_min, time_delay_max)
+        for i in tqdm(range(sleepDelay), desc='sleep ', bar_format='{desc}: {n_fmt}/{total_fmt}'):
+            time.sleep(1)
+        print()
+    print('Done! Subscribe if you want more https://t.me/legalcrypt')
+main()
